b29449708496e499fd17ac6f88a139605a2ebcd5
niko <niko@401c1991-876b-4f27-981d-9a9531d83472>
2008-11-11 13:10:33 +0000

    ! Comments disappears after posting comment

--- Sources/IssueView.php
+++ Sources/IssueView.php
@@ -171,6 +171,9 @@ function IssueView()
 	if (isset($_REQUEST['view']) && $_REQUEST['view'] == 'log')
 		$context['current_view'] = 'log';
 
+	if ($context['current_view'] == 'comments')
+		$context['page_index'] = constructPageIndex(project_get_url(array('issue' => $issue . '.%d')), $_REQUEST['start'], $context['current_issue']['replies'], $context['comments_per_page'], true);
+
 	prepareComments($context['current_view'] == 'comments');
 
 	if ($context['current_view'] == 'comments')
@@ -203,8 +206,6 @@ function IssueViewComments()
 			array('id_issue', 'id_member')
 		);
 	}
-
-	$context['page_index'] = constructPageIndex(project_get_url(array('issue' => $issue . '.%d')), $_REQUEST['start'], $context['current_issue']['replies'], $context['comments_per_page'], true);
 
 	loadAttachmentData();
 
