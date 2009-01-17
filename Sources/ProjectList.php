bb18d12dfb2701ea9d3fa7382acf0181b0a986e2
niko <niko@401c1991-876b-4f27-981d-9a9531d83472>
2009-01-17 14:07:37 +0000

    Updated copyright years

--- Sources/ProjectList.php
+++ Sources/ProjectList.php
@@ -6,7 +6,7 @@
 * =============================================================================== *
 * Software Version:           SMF Project Tools 0.2                               *
 * Software by:                Niko Pahajoki (http://www.madjoki.com)              *
-* Copyright 2007-2008 by:     Niko Pahajoki (http://www.madjoki.com)              *
+* Copyright 2007-2009 by:     Niko Pahajoki (http://www.madjoki.com)              *
 * Support, News, Updates at:  http://www.madjoki.com                              *
 ***********************************************************************************
 * This program is free software; you may redistribute it and/or modify it under   *
