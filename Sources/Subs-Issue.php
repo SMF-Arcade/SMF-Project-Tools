d62c631f4f29685f360ab67037a4c7b7e36c8830
niko <niko@401c1991-876b-4f27-981d-9a9531d83472>
2009-07-03 07:57:47 +0000

    ! missed one change

--- Sources/Subs-Issue.php
+++ Sources/Subs-Issue.php
@@ -929,7 +929,7 @@ function getIssueList($start = 0, $num_issues, $order = 'i.updated DESC', $where
 		$return[] = array(
 			'id' => $row['id_issue'],
 			'name' => $row['subject'],
-			'link' => '<a href="' . project_get_url(array('issue' => $row['id_issue'] . '.0')) . '">' . $row['subject'] . '</a>',
+			'link' => '<a href="' . project_get_url(array('issue' => $row['id_issue'] . '.0'), $row['id_project']) . '">' . $row['subject'] . '</a>',
 			'href' => project_get_url(array('issue' => $row['id_issue'] . '.0'), $row['id_project']),
 			'category' => array(
 				'id' => $row['id_category'],
