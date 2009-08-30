532a18dd8afedb659d02c5a5d6c7ee1627c46614
niko <niko@401c1991-876b-4f27-981d-9a9531d83472>
2009-08-30 14:18:37 +0000

    more fixes

--- Sources/Subs-Issue.php
+++ Sources/Subs-Issue.php
@@ -119,7 +119,7 @@ function loadIssue()
 		'private' => !empty($row['private_issue']),
 	);
 
-	if (!$user_info['is_admin'] && count(array_intersect($context['current_issue']['versions'], $user_info['project_allowed_versions'])) == 0)
+	if (!$user_info['is_admin'] && count(array_intersect(array_keys($context['current_issue']['versions']), $user_info['project_allowed_versions'])) == 0)
 		$context['project_error'] = 'issue_not_found';
 	// If this is private issue are you allowed to see it?
 	elseif ($context['current_issue']['private'] && !$user_info['is_admin'] && !$context['project']['is_developer'] && $user_info['id'] != $row['id_reporter'] && !projectAllowedTo('issue_view_private'))
