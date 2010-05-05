29a8a60d4fc05daa4157d19e4dd8c9e77b91a233
niko <niko@401c1991-876b-4f27-981d-9a9531d83472>
2010-05-05 19:04:03 +0000

    ! Incorrect urls in roadmap

--- ptinstall/Database.php
+++ ptinstall/Database.php
@@ -4,7 +4,7 @@
 ***********************************************************************************
 * SMF Project Tools                                                               *
 * =============================================================================== *
-* Software Version:           SMF Project Tools 0.4                               *
+* Software Version:           SMF Project Tools 0.4.3                             *
 * Software by:                Niko Pahajoki (http://www.madjoki.com)              *
 * Copyright 2007-2009 by:     Niko Pahajoki (http://www.madjoki.com)              *
 * Support, News, Updates at:  http://www.madjoki.com                              *
@@ -25,7 +25,7 @@ global $project_version, $addSettings, $permissions, $tables;
 if (!defined('SMF'))
 	die('Hacking attempt...');
 
-$project_version = '0.4';
+$project_version = '0.4.3';
 
 $addSettings = array(
 	'issuesPerPage' => array(25, false),
