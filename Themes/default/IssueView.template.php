ae6cf533bfe26836c410536de4ee2bbee273a5a5
niko <niko@401c1991-876b-4f27-981d-9a9531d83472>
2010-05-06 00:36:35 +0000

    ! Bug image not aligned properly [Issue #337]

--- Themes/default/IssueView.template.php
+++ Themes/default/IssueView.template.php
@@ -28,7 +28,7 @@ function template_issue_view_above()
 	<a name="com', $context['current_issue']['comment_first'], '"></a>
 	<div id="issue_comments" class="floatleft">
 		<h3 class="catbg"><span class="left"></span><span class="right"></span>
-			<img src="', $settings['default_images_url'], '/', $context['current_issue']['tracker']['image'], '" alt="', $context['current_issue']['tracker']['name'], '" width="20" />
+			<img src="', $settings['default_images_url'], '/', $context['current_issue']['tracker']['image'], '" style="vertical-align: middle;" alt="', $context['current_issue']['tracker']['name'], '" width="20" />
 			<span>', $txt['issue'], ': ', $context['current_issue']['name'], '</span>
 		</h3>';
 
