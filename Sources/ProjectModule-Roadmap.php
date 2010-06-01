ad091de7e7704ba137d1ebdf5c2f53623faf7db9
niko <niko@401c1991-876b-4f27-981d-9a9531d83472>
2010-06-01 19:39:17 +0000

    more documentation added

--- Sources/ProjectModule-Roadmap.php
+++ Sources/ProjectModule-Roadmap.php
@@ -1,24 +1,12 @@
 <?php
-/**********************************************************************************
-* ProjectModule-Roadmap.php                                                       *
-***********************************************************************************
-* SMF Project Tools                                                               *
-* =============================================================================== *
-* Software Version:           SMF Project Tools 0.5                               *
-* Software by:                Niko Pahajoki (http://www.madjoki.com)              *
-* Copyright 2007-2010 by:     Niko Pahajoki (http://www.madjoki.com)              *
-* Support, News, Updates at:  http://www.madjoki.com                              *
-***********************************************************************************
-* This program is free software; you may redistribute it and/or modify it under   *
-* the terms of the provided license as published by Simple Machines LLC.          *
-*                                                                                 *
-* This program is distributed in the hope that it is and will be useful, but      *
-* WITHOUT ANY WARRANTIES; without even any implied warranty of MERCHANTABILITY    *
-* or FITNESS FOR A PARTICULAR PURPOSE.                                            *
-*                                                                                 *
-* See the "license.txt" file for details of the Simple Machines license.          *
-* The latest version can always be found at http://www.simplemachines.org.        *
-**********************************************************************************/
+/**
+ * Contains roadmap module for projects 
+ *
+ * @package issuetracker
+ * @version 0.5
+ * @license htttp://download.smfproject.net/license.php New-BSD
+ * @since 0.5
+ */
 
 if (!defined('SMF'))
 	die('Hacking attempt...');
