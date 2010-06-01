626408d7b2714eaa94e64856b778959a7887962e
niko <niko@401c1991-876b-4f27-981d-9a9531d83472>
2010-06-01 20:30:16 +0000

    updating documentation

--- projectstandalone.php
+++ projectstandalone.php
@@ -1,32 +1,48 @@
 <?php
+/**
+ * This file is for running Project Tools in standalone mode,
+ * where Project Tools is not located inside forum.
+ *
+ * Meant for having Project Tools inside site rather than inside forum
+ *
+ * Example settings:
+ *
+ * INSERT INTO `smf_settings` (`variable` ,`value`)
+ * 	VALUES
+ * ('projectStandalone', '2'),
+ * ('projectStandaloneUrl', 'http://www.smfarcade.info/projects')
+ *
+ * projectStandalone:
+ * 	1 - No "SEO" urls
+ *	2 - "SEO" urls (not supported yet)
+ *
+ *	Example .htaccess rules for "SEO" urls
+ *		(not supported yet)
+ * 
+ *	projectStandaloneUrl:
+ *
+ *		Base url for SEO urls.
+ *
+ *		eg. http://www.smfarcade.info/projects
+ *
+ *	url to renamed projectstandalone.php for non SEO urls
+ *	eg. http://www.smfarcade.info/projects/index.php
+ *
+ * @package core
+ * @version 0.5
+ * @license http://download.smfproject.net/license.php New-BSD
+ * @since 0.1
+ */
 
 /*
-	This file is for running Project Tools in standalone mode, where Project Tools is not located inside forum.
-	Meant for having Project Tools inside site rather than inside forum
+	
+	
 
 	Eexample settings
 
-		INSERT INTO `smf_settings` (`variable` ,`value`)
-		VALUES
-			('projectStandalone', '2'),
-			('projectStandaloneUrl', 'http://www.smfarcade.info/projects')
+		
 
-	projectStandalone:
-		1 - No "SEO" urls
-		2 - "SEO" urls (not supported yet)
-
-	Example .htaccess rules for "SEO" urls
-		(not supported yet)
-
-	projectStandaloneUrl:
-
-		Base url for SEO urls.
-
-			eg. http://www.smfarcade.info/wiki
-
-		url to projectstandalone.php for non SEO urls
-
-			eg. http://www.smfarcade.info/projects/index.php
+	
 
 */
 
@@ -40,7 +56,7 @@ require_once(dirname(dirname(__FILE__)) . '/SSI.php');
 loadIssue();
 
 if (isset($context['project_error']))
-	fatal_lang_error($context['project_error'], false);
+    fatal_lang_error($context['project_error'], false);
 
 // Main thing
 Projects(true);
