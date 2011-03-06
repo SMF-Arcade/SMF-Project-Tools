7df9833e7f616a86a5fbbd63bf751f7eb87e0913
niko <niko@401c1991-876b-4f27-981d-9a9531d83472>
2011-03-06 19:51:56 +0000

    + Frontpage working again

--- Sources/ProjectTools/Main.php
+++ Sources/ProjectTools/Main.php
@@ -24,6 +24,16 @@ class ProjectTools_Main
 	/**
 	 *
 	 */
+	static protected $areas;
+	
+	/**
+	 *
+	 */
+	static protected $current_area;
+	
+	/**
+	 *
+	 */
 	static protected function fix_url()
 	{
 		global $issue;
@@ -65,6 +75,22 @@ class ProjectTools_Main
 			
 			if (!isset($_REQUEST['sa']))
 				$_REQUEST['sa'] = 'view';
+		}
+	}
+	
+	/**
+	 *
+	 */
+	static private function CreateAreas($areas)
+	{
+		foreach ($areas as $id => $area)
+		{
+			if (!empty($area['project_permission']) && !ProjectTools_Project::getCurrent()->allowedTo($area['project_permission']))
+				continue;
+			elseif (!empty($area['permission']) && !allowedTo($area['permission']))
+				continue;
+		
+			self::$areas[$id] = $area;
 		}
 	}
 	
@@ -370,12 +396,8 @@ class ProjectTools_Main
 		$context['issues_per_page'] = !empty($modSettings['issuesPerPage']) ? $modSettings['issuesPerPage'] : 25;
 		$context['comments_per_page'] = !empty($modSettings['commentsPerPage']) ? $modSettings['commentsPerPage'] : 20;
 	
-		if ($mode == '')
-		{
-
-		}
 		// In SMF (SSI, etc)
-		elseif ($mode == 'smf')
+		if ($mode == 'smf')
 		{
 			loadTemplate(false, array('project'));
 		}
@@ -428,7 +450,7 @@ class ProjectTools_Main
 		if (ProjectTools_Project::getCurrent())
 		{		
 			self::fix_url();
-			ProjectTools_ProjectPage::Main();
+			self::ProjectMain();
 			return;
 		}
 		
@@ -443,6 +465,121 @@ class ProjectTools_Main
 		call_user_func(array('ProjectTools_Main', $subActions[$_REQUEST['sa']][0]));
 		
 		return;	
+	}
+	
+	/**
+	 *
+	 */
+	static public function ProjectMain()
+	{
+		global $context, $txt, $settings;
+			
+		$context['active_project_modules'] = array();
+			
+		// Load Modules
+		foreach (ProjectTools_Project::getCurrent()->modules as $id)
+		{
+			$module = ProjectTools_Extensions::getModule($id);
+			
+			if ($module)
+				$context['active_project_modules'][$id] = new $module['class_name'](ProjectTools_Project::getCurrent());
+		}
+		
+		//
+		$project_areas = array(
+			'main' => array(
+				'title' => $txt['project'],
+				'href' => project_get_url(array('project' => ProjectTools_Project::getCurrent()->id)),
+				'callback' => array('ProjectTools_ProjectPage', 'Main'),
+				'hide_linktree' => true,
+				'order' => 'first',
+			),
+		);
+		
+		//
+		foreach ($context['active_project_modules'] as $id => $module)
+		{
+			$area = $module->RegisterArea();
+			$project_areas[$area['id']] = $area;
+		}
+		
+		self::CreateAreas($project_areas);
+		unset($project_areas);
+		
+		// Tabs
+		$context['project_tabs'] = array(
+			'title' => ProjectTools_Project::getCurrent()->name,
+			'description' => ProjectTools_Project::getCurrent()->description,
+			'tabs' => array(),
+		);
+		
+		if (empty($_REQUEST['area']) || !isset(self::$areas[$_REQUEST['area']]))
+			$_REQUEST['area'] = 'main';
+			
+		if (empty($_REQUEST['sa']))
+			$_REQUEST['sa'] = 'main';
+			
+		self::$current_area = &self::$areas[$_REQUEST['area']];
+		
+		// Create Tabs
+		foreach (self::$areas as $id => &$area)
+		{
+			$area['href'] = isset($area['href']) ? $area['href'] : project_get_url(array('project' => ProjectTools_Project::getCurrent()->id, 'area' => $id));
+			
+			$context['project_tabs']['tabs'][$id] = array(
+				'title' => $area['title'],
+				'href' => $area['href'],
+				'is_selected' => $area === self::$current_area,
+				'hide_linktree' => !empty($area['hide_linktree']),
+				'order' => $area['order'],
+			);
+		}
+	
+		// Sort tabs to correct order
+		uksort($context['project_tabs']['tabs'], 'projectTabSort');
+			
+		// Call Initialize View function
+		//if (isset($context['current_project_module']) && method_exists($context['current_project_module'], 'beforeSubaction'))
+		//	$context['current_project_module']->beforeSubaction($_REQUEST['sa']);
+			
+		// Linktree
+		$context['linktree'][] = array(
+			'name' => strip_tags(ProjectTools_Project::getCurrent()->name),
+			'url' => project_get_url(array('project' => ProjectTools_Project::getCurrent()->id)),
+		);
+		
+		if (empty(self::$current_area['hide_linktree']))
+			$context['linktree'][] = array(
+				'name' => self::$current_area['title'],
+				'url' => self::$current_area['href'],
+			);
+		
+		/*
+		if (isset($context['current_project_module']->subTabs[$_REQUEST['sa']]))
+		{
+			$context['current_project_module']->subTabs[$_REQUEST['sa']]['is_selected'] = true;
+			
+			if (empty($context['current_project_module']->subTabs[$_REQUEST['sa']]['hide_linktree']))
+				$context['linktree'][] = array(
+					'name' => $context['current_project_module']->subTabs[$_REQUEST['sa']]['title'],
+					'url' => $context['current_project_module']->subTabs[$_REQUEST['sa']]['href'],
+				);
+			
+			$context['project_sub_tabs'] = $context['current_project_module']->subTabs;
+		}*/
+		
+		// Template
+		loadTemplate('Project', array('project'));
+		
+		if (!isset($_REQUEST['xml']))
+		{
+			$context['template_layers'][] = 'project';
+			
+			$context['html_headers'] .= '
+			<script language="JavaScript" type="text/javascript" src="' . $settings['default_theme_url'] . '/scripts/project.js"></script>';
+		}
+		
+		call_user_func(self::$current_area['callback'], array($_REQUEST['sa']));
 	}
 	
 	/**
--- Sources/ProjectTools/ProjectPage.php
+++ Sources/ProjectTools/ProjectPage.php
@@ -19,142 +19,235 @@ class ProjectTools_ProjectPage
 	/**
 	 *
 	 */
-	static protected $areas;
-	
-	/**
-	 *
-	 */
-	static protected $current_area;
-	
-	/**
-	 *
-	 */
 	static public function Main()
 	{
-		global $context, $txt, $settings;
-			
-		$context['active_project_modules'] = array();
-			
-		// Load Modules
-		foreach (ProjectTools_Project::getCurrent()->modules as $id)
-		{
-			$module = ProjectTools_Extensions::getModule($id);
-			
-			if ($module)
-				$context['active_project_modules'][$id] = new $module['class_name'](ProjectTools_Project::getCurrent());
-		}
-		
-		//
-		$project_areas = array(
-			'main' => array(
-				'title' => $txt['project'],
-				'href' => project_get_url(array('project' => ProjectTools_Project::getCurrent()->id)),
-				'callback' => array(get_class(), 'ViewPage'),
-				'hide_linktree' => true,
-				'order' => 'first',
-			),
+		$subActions = array(
+			'main' => array(__CLASS__, 'Frontpage'),
+			'subscribe' => array(__CLASS__, 'Subscribe'),
 		);
 		
-		//
-		foreach ($context['active_project_modules'] as $id => $module)
-		{
-			$area = $module->RegisterArea();
-			$project_areas[$area['id']] = $area;
-		}
-		
-		self::CreateAreas($project_areas);
-		unset($project_areas);
-		
-		// Tabs
-		$context['project_tabs'] = array(
-			'title' => ProjectTools_Project::getCurrent()->name,
-			'description' => ProjectTools_Project::getCurrent()->description,
-			'tabs' => array(),
-		);
-		
-		if (empty($_REQUEST['area']) || !isset(self::$areas[$_REQUEST['area']]))
-			$_REQUEST['area'] = 'main';
-			
-		if (empty($_REQUEST['sa']))
+		if (!isset($_REQUEST['sa']) || !isset($subActions[$_REQUEST['sa']]))
 			$_REQUEST['sa'] = 'main';
 			
-		self::$current_area = &self::$areas[$_REQUEST['area']];
-		
-		// Create Tabs
-		foreach (self::$areas as $id => &$area)
-		{
-			$area['href'] = isset($area['href']) ? $area['href'] : project_get_url(array('project' => ProjectTools_Project::getCurrent()->id, 'area' => $id));
-			
-			$context['project_tabs']['tabs'][$id] = array(
-				'title' => $area['title'],
-				'href' => $area['href'],
-				'is_selected' => $area === self::$current_area,
-				'hide_linktree' => !empty($area['hide_linktree']),
-				'order' => $area['order'],
-			);
-		}
-	
-		// Sort tabs to correct order
-		uksort($context['project_tabs']['tabs'], 'projectTabSort');
-			
-		// Call Initialize View function
-		//if (isset($context['current_project_module']) && method_exists($context['current_project_module'], 'beforeSubaction'))
-		//	$context['current_project_module']->beforeSubaction($_REQUEST['sa']);
-			
-		// Linktree
-		$context['linktree'][] = array(
-			'name' => strip_tags(ProjectTools_Project::getCurrent()->name),
-			'url' => project_get_url(array('project' => ProjectTools_Project::getCurrent()->id)),
-		);
-		
-		if (empty(self::$current_area['hide_linktree']))
-			$context['linktree'][] = array(
-				'name' => self::$current_area['title'],
-				'url' => self::$current_area['href'],
-			);
-		
-		/*
-		if (isset($context['current_project_module']->subTabs[$_REQUEST['sa']]))
-		{
-			$context['current_project_module']->subTabs[$_REQUEST['sa']]['is_selected'] = true;
-			
-			if (empty($context['current_project_module']->subTabs[$_REQUEST['sa']]['hide_linktree']))
-				$context['linktree'][] = array(
-					'name' => $context['current_project_module']->subTabs[$_REQUEST['sa']]['title'],
-					'url' => $context['current_project_module']->subTabs[$_REQUEST['sa']]['href'],
-				);
-			
-			$context['project_sub_tabs'] = $context['current_project_module']->subTabs;
-		}*/
-		
-		// Template
-		loadTemplate('Project', array('project'));
-		
-		if (!isset($_REQUEST['xml']))
-		{
-			$context['template_layers'][] = 'project';
-			
-			$context['html_headers'] .= '
-			<script language="JavaScript" type="text/javascript" src="' . $settings['default_theme_url'] . '/scripts/project.js"></script>';
-		}
-		
-		call_user_func(self::$current_area['callback'], array($_REQUEST['sa']));
+		call_user_func($subActions[$_REQUEST['sa']]);
 	}
 	
 	/**
-	 *
+	 * Main project page
 	 */
-	static private function CreateAreas($areas)
+	static public function Frontpage()
 	{
-		foreach ($areas as $id => $area)
-		{
-			if (!empty($area['project_permission']) && !ProjectTools_Project::getCurrent()->allowedTo($area['project_permission']))
-				continue;
-			elseif (!empty($area['permission']) && !allowedTo($area['permission']))
-				continue;
+		global $context, $modSettings, $smcFunc, $sourcedir, $user_info, $txt, $project;
+	
+		// Canonical url for search engines
+		$context['canonical_url'] = project_get_url(array('project' => $project));
 		
-			self::$areas[$id] = $area;
+		$context['can_subscribe'] = !$user_info['is_guest'];
+		$context['can_report_issues'] = projectAllowedTo('issue_report');
+	
+		if (!$user_info['is_guest'] && !empty($modSettings['project_maxEventID']))
+		{
+			// We can't know they read it if we allow prefetches.
+			if (isset($_SERVER['HTTP_X_MOZ']) && $_SERVER['HTTP_X_MOZ'] == 'prefetch')
+			{
+				ob_end_clean();
+				header('HTTP/1.1 403 Prefetch Forbidden');
+				die;
+			}
+	
+			$smcFunc['db_insert']('replace',
+				'{db_prefix}log_projects',
+				array('id_project' => 'int', 'id_member' => 'int', 'id_event' => 'int',),
+				array($project, $user_info['id'], $modSettings['project_maxEventID'],),
+				array('id_project', 'id_member')
+			);
+	
+			$request = $smcFunc['db_query']('', '
+				SELECT sent
+				FROM {db_prefix}log_notify_projects
+				WHERE id_project = {int:project}
+					AND id_member = {int:current_member}
+				LIMIT 1',
+				array(
+					'project' => $project,
+					'current_member' => $user_info['id'],
+				)
+			);
+			$context['is_subscribed'] = $smcFunc['db_num_rows']($request) != 0;
+			
+			// If user is subscribed reset sent status
+			if ($context['is_subscribed'])
+			{
+				list ($sent) = $smcFunc['db_fetch_row']($request);
+				if (!empty($sent))
+				{
+					$smcFunc['db_query']('', '
+						UPDATE {db_prefix}log_notify_projects
+						SET sent = {int:is_sent}
+						WHERE id_project = {int:project}
+							AND id_member = {int:current_member}',
+						array(
+							'project' => $project,
+							'current_member' => $user_info['id'],
+							'is_sent' => 0,
+						)
+					);
+				}
+			}
+			$smcFunc['db_free_result']($request);
 		}
+		
+		$frontpage_blocks = array();
+		
+		// Let Modules register Frontpage blocks
+		if (!empty($context['active_project_modules']))
+		{
+			foreach ($context['active_project_modules'] as $module)
+				if (method_exists($module, 'RegisterProjectFrontpageBlocks'))
+					$module->RegisterProjectFrontpageBlocks($frontpage_blocks);
+		}
+		
+		$context['project_blocks'] = array();
+		
+		// Load Frontpage Blocks
+		if (!empty($frontpage_blocks))
+		{
+			foreach ($frontpage_blocks as $id => $block)
+			{
+				if (empty($block['show']))
+					continue;
+				
+				$context['project_blocks'][$id] = array(
+					'title' => is_array($block['title']) ? vsprintf($txt[$block['title'][0]], $block['title'][1]) : $txt[$block['title']],
+					'href' => isset($block['href']) ? $block['href'] : '',
+					'template' => isset($block['template']) ? $block['template'] : '',
+					'data' => call_user_func_array($block['data_function'], $block['data_parameters']),
+				);
+			}
+		}
+	
+		loadTimeline($project);
+	
+		// Template
+		loadTemplate('ProjectView');
+		$context['sub_template'] = 'project_view';
+		$context['page_title'] = sprintf($txt['project_title'], ProjectTools_Project::getCurrent()->name);
+	}
+	
+	/**
+	 * Subscribe to project
+	 */
+	static public function Subscribe()
+	{
+		global $context, $smcFunc, $sourcedir, $user_info, $txt, $project, $issue;
+	
+		checkSession('get');
+	
+		if ($user_info['is_guest'])
+			fatal_lang_error('cannot_project_subscribe');
+	
+		if (!empty($issue))
+			return self::SubscribeIssue();
+	
+		$request = $smcFunc['db_query']('', '
+			SELECT id_project
+			FROM {db_prefix}log_notify_projects
+			WHERE id_project = {int:project}
+				AND id_member = {int:current_member}',
+			array(
+				'project' => $project,
+				'current_member' => $user_info['id'],
+			)
+		);
+	
+		$row = $smcFunc['db_fetch_assoc']($request);
+	
+		if (!$row)
+			$smcFunc['db_insert']('',
+				'{db_prefix}log_notify_projects',
+				array(
+					'id_project' => 'int',
+					'id_issue' => 'int',
+					'id_member' => 'int',
+					'sent' => 'int',
+				),
+				array(
+					$project,
+					0,
+					$user_info['id'],
+					0,
+				),
+				array('id_project', 'id_issue', 'id_member')
+			);
+		else
+			$smcFunc['db_query']('', '
+				DELETE FROM {db_prefix}log_notify_projects
+				WHERE id_project = {int:project}
+					AND id_member = {int:current_member}',
+				array(
+					'project' => $project,
+					'current_member' => $user_info['id'],
+				)
+			);
+	
+		$smcFunc['db_free_result']($request);
+	
+		redirectexit(project_get_url(array('project' => $project)));
+	}
+	
+	/**
+	 * Subscribe to issue
+	 *
+	 * @todo Move to IssueTracker modules
+	 */
+	static public function SubscribeIssue()
+	{
+		global $context, $smcFunc, $sourcedir, $user_info, $txt, $project, $issue;
+	
+		$request = $smcFunc['db_query']('', '
+			SELECT id_project
+			FROM {db_prefix}log_notify_projects
+			WHERE id_issue = {int:issue}
+				AND id_member = {int:current_member}',
+			array(
+				'issue' => $issue,
+				'current_member' => $user_info['id'],
+			)
+		);
+	
+		$row = $smcFunc['db_fetch_assoc']($request);
+	
+		if (!$row)
+			$smcFunc['db_insert']('',
+				'{db_prefix}log_notify_projects',
+				array(
+					'id_project' => 'int',
+					'id_issue' => 'int',
+					'id_member' => 'int',
+					'sent' => 'int',
+				),
+				array(
+					0,
+					$issue,
+					$user_info['id'],
+					0,
+				),
+				array('id_project', 'id_issue', 'id_member')
+			);
+		else
+			$smcFunc['db_query']('', '
+				DELETE FROM {db_prefix}log_notify_projects
+				WHERE id_issue = {int:issue}
+					AND id_member = {int:current_member}',
+				array(
+					'issue' => $issue,
+					'current_member' => $user_info['id'],
+				)
+			);
+	
+		$smcFunc['db_free_result']($request);
+	
+		redirectexit(project_get_url(array('issue' => $issue . '.0')));
 	}
 }
 
